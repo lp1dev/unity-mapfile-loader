8ee62787c0cfa2021655788efd6a60090b1003df
Lupin <mail@lupin.me>
2015-10-13 22:35:33 +0200

    first commtig

---
+++ GameMap.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Text;
+using System.IO;
+
+public class Position
+{
+    public int x, y;
+
+    public Position(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class GameMap{
+    public int wallsHeight;
+    public Hashtable mapList = new Hashtable();
+    public Position spawn, size, end;
+    public ArrayList walls;
+    public ArrayList floor;
+    public ArrayList doors;
+    public ArrayList monsters;
+    public string endPhrase;
+
+    public void processLine(int index, string buffer) {
+        for (int i = 0; i < buffer.Length; i++)
+            switch (buffer[i]){
+                case '0':
+                    walls.Add(new Position(index, i));
+                    break;
+                case 'X':
+                    this.spawn = new Position(index, i);
+                    break;
+                case ' ':
+                    floor.Add(new Position(index, i));
+                    break;
+                case 'D':
+                    doors.Add(new Position(index, i));
+                    break;
+                case 'M':
+                    monsters.Add(new Position(index, i));
+                    break;
+                case 'E':
+                    this.end = new Position(index, i);
+                    break;
+            }
+    }
+
+    public bool load(string filename)
+    {
+        try
+        {
+            int i = 0;
+            string buffer;
+            string[] mapSize;
+
+            walls = new ArrayList();
+            floor = new ArrayList();
+            doors = new ArrayList();
+            monsters = new ArrayList();
+            StreamReader streamReader = new StreamReader(filename, Encoding.Default);
+            buffer = streamReader.ReadLine();
+            mapSize = buffer.Split(';');
+            this.size = new Position(int.Parse(mapSize[0]), int.Parse(mapSize[1]));
+            this.wallsHeight = int.Parse(mapSize[2]);
+            while (((buffer = streamReader.ReadLine()) != null) && i < this.size.y)
+                processLine(i++, buffer);
+            this.endPhrase = buffer;
+            streamReader.Close();
+            return true;
+        }
+        catch (System.Exception)
+        {
+            return false;
+        }
+    }
+
+    public GameMap(int map_number)
+    {
+        mapList[0] = "Assets\\Maps\\map1.txt";
+        mapList[1] = "Assets\\Maps\\map2.txt";
+        this.load(mapList[map_number].ToString());
+    }
+}
---
+++ mapGeneration.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mapGeneration : MonoBehaviour
+{
+    public int selectedMap = 0;
+    public GameMap gameMap;
+    private GameObject player;
+
+    void generateCube(int x, int y, int z, Material material)
+    {
+        Vector3 spawnLocation = new Vector3(x, z, y);
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.GetComponent<Renderer>().material = material;
+        cube.transform.position = spawnLocation;
+    }
+
+    void generateWalls(){
+        Material wallsMaterial = Resources.Load("Materials/wallMaterial") as Material;
+        foreach (Position position in gameMap.walls)
+            for (int i = 0; i < gameMap.wallsHeight; i++)
+                generateCube(position.x, position.y, 1 + i, wallsMaterial);
+    }
+
+    void generateFloor()
+    {
+        Material floorMaterial = Resources.Load("Materials/floorMaterial") as Material;
+        foreach (Position position in gameMap.floor)
+            generateCube(position.x, position.y, 0, floorMaterial);
+        generateCube(gameMap.spawn.x, gameMap.spawn.y, 0, floorMaterial);
+        generateCube(gameMap.end.x, gameMap.end.y, 0, floorMaterial);
+    }
+
+    void generateDoors()
+    {
+        Material doorMaterial = Resources.Load("Materials/doorMaterial") as Material;
+        foreach (Position position in gameMap.doors)
+            for (int i = 0; i < gameMap.wallsHeight; i++)
+                generateCube(position.x, position.y, 1 + i, doorMaterial);
+    }
+
+    void generateMonsters()
+    {
+        //Material doorMaterial = Resources.Load("Materials/doorMaterial") as Material;
+        foreach (Position position in gameMap.monsters)
+        {
+            GameObject monster = (GameObject)Instantiate(Resources.Load("Prefabs/ghost"));
+            monster.transform.position = new Vector3(position.x, 1.5F, position.y);
+            generateCube(position.x, position.y, 0, Resources.Load("Materials/floorMaterial") as Material);
+        }
+    }
+
+    public void generateMap(int mapNumber)
+    {
+        gameMap = new GameMap(mapNumber);
+        player = GameObject.Find("character"); //Generating the character
+        player.transform.position = new Vector3(gameMap.spawn.x, 1, gameMap.spawn.y);
+        generateWalls();
+        generateFloor();
+        generateDoors();
+        generateMonsters();
+        //Generating end of level
+        GameObject levelEnd = (GameObject)Instantiate(Resources.Load("Prefabs/levelEnd"));
+        levelEnd.transform.position = new Vector3(gameMap.end.x, 1.5F, gameMap.end.y);
+        GameObject subtitle = GameObject.Find("subtitle");
+        subtitle.GetComponent<Text>().text = gameMap.endPhrase;
+    }
+
+    void Start () {
+        generateMap(selectedMap);
+    }
+
+	void Update () {
+	}
+}
